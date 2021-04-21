dc22cf292c68e65d1a42b1131a7b773dff6eb498
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-04-21 22:13:52 +0900

    day-2_2

--- week1/day2_for/10871_X보다작은수.cpp
+++ week1/day2_for/10871_X보다작은수.cpp
@@ -2,31 +2,13 @@
 using namespace std;
 
 int main(){
-	int N, X, listNum, listSize;
+	int N, X, listNum;
 	cin >> N >> X;
-	int Nlist[N];
-	
 	for(int i=0; i<N; i++){
 		cin >> listNum;
-		Nlist[i] = listNum;
-	}
-	
-	for(int k=0; k<N; k++){
-		if(Nlist[k] < X){
-			listSize += 1;	
+		if(X > listNum){
+			cout << listNum << " ";
 		}
-	}
-	
-	int Plist[listSize];
-	
-	for(int j=0; j<N; j++){
-		if(Nlist[j] < X){
-			Plist[j] = Nlist[j];	
-		}
-	}
-	
-	for(int h=0; h<listSize; h++){
-		cout << Plist[h] << " ";
 	}
 }
 
